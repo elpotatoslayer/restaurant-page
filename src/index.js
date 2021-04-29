96b291148e961b2c05514abf8a392c65a855ba9e
Ralph <ralphoburu@gmail.com>
2021-04-29 11:11:47 +0300

    Add main display

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import navbar from './utils/pages/nav';
 import food from './utils/img/board.jpg';
 import './utils/css/style.css';
+import home from './utils/pages/home';
 
 
 const display = () => {
   const content = document.querySelector('#content');
 
   content.innerHTML = `
-  <div class="bg-pic">
-  
-  </div>
   <div>
     ${navbar()}
+    ${home()}
   </div>
   <div id="display-text">
-
   </div>
 `;
 };
 
+const loadEventListers = () => {
+  const switchTabs = event => {
+    const content = document.getElementById('display-text');
+    switch (event.target.element) {
+      case 'Home':
+        content.innerHTML = home();
+        break;
+    
+      default:
+        break;
+    }
+  }
+}
 
+// loadEventListers();
 display();
 
